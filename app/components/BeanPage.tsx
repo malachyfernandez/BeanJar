60e6b46f89c698879ad85e6fb4c386a71093e794
malachyfernandez <jag_pyloric.0j@icloud.com>
2026-03-14 08:22:20 -0400

    change

--- app/components/BeanPage.tsx
+++ app/components/BeanPage.tsx
@@ -48,102 +48,102 @@ const BeanPage = ({
     // New Stufff
     // ------------------------------------------------------------------------
 
-    const freinds = ["user_38GcBXAETIhkNg1rIBxN85O1u33"];
+    // const freinds = ["user_38GcBXAETIhkNg1rIBxN85O1u33"];
 
-    const [listItems, setListItems] = useUserVariable<string[]>({
-        key: "listItems",
-        defaultValue: [],
-        privacy: freinds,
-    });
+    // const [listItems, setListItems] = useUserVariable<string[]>({
+    //     key: "listItems",
+    //     defaultValue: [],
+    //     privacy: freinds,
+    // });
 
-    const newItem = () => {
-        if (!listItems.value) return;
-        setListItems([...listItems.value, `New Item ${listItems.value.length + 1}`]);
-    };
+    // const newItem = () => {
+    //     if (!listItems.value) return;
+    //     setListItems([...listItems.value, `New Item ${listItems.value.length + 1}`]);
+    // };
 
-    const removeItem = (index: number) => {
-        if (!listItems.value) return;
-        setListItems(listItems.value.filter((_, i) => i !== index));
-    };
+    // const removeItem = (index: number) => {
+    //     if (!listItems.value) return;
+    //     setListItems(listItems.value.filter((_, i) => i !== index));
+    // };
 
 
 
-    const freindsList = useUserVariableGet<string[][]>({
-        key: "listItems",
-        returnTop: 3,
-    });
+    // const freindsList = useUserVariableGet<string[][]>({
+    //     key: "listItems",
+    //     returnTop: 3,
+    // });
 
-    const FreindListChange = useUserVariablePrivacy({
-        key: "listItems",
-    });
+    // const FreindListChange = useUserVariablePrivacy({
+    //     key: "listItems",
+    // });
 
 
     return (
-        // <ContainerCol className=' w-full h-full items-center justify-center'>
-        //     <BeanContainer numberOfBeans={0} beanText={beanText.value} setBeanText={setBeanText} />
+        <ContainerCol className=' w-full h-full items-center justify-center'>
+            <BeanContainer numberOfBeans={0} beanText={beanText.value} setBeanText={setBeanText} />
 
-        //     <TouchableOpacity className='bg-slate-700 p-3 rounded absolute bottom-10' onPress={() => signOut()}>
-        //         <PoppinsText>Sign Out</PoppinsText>
-        //     </TouchableOpacity>
-        // </ContainerCol>
+            <TouchableOpacity className='bg-slate-700 p-3 rounded absolute bottom-10' onPress={() => signOut()}>
+                <PoppinsText>Sign Out</PoppinsText>
+            </TouchableOpacity>
+        </ContainerCol>
 
-        <View className='justify-between w-full h-full p-10'>
+        // <View className='justify-between w-full h-full p-10'>
             
-            <ContainerCol>
-                {/* remove freind button */}
-                <TouchableOpacity className='border-slate-700 border p-3 rounded w-48 items-center hover:bg-slate-600' onPress={() => FreindListChange("PRIVATE")}>
-                    <PoppinsText>Remove Freind</PoppinsText>
-                </TouchableOpacity>
-                {/* add friend button */}
-                <TouchableOpacity className='border-slate-700 border p-3 rounded w-48 items-center hover:bg-slate-600' onPress={() => FreindListChange(freinds)}>
-                    <PoppinsText>Add Freind</PoppinsText>
-                </TouchableOpacity>
+        //     <ContainerCol>
+        //         {/* remove freind button */}
+        //         <TouchableOpacity className='border-slate-700 border p-3 rounded w-48 items-center hover:bg-slate-600' onPress={() => FreindListChange("PRIVATE")}>
+        //             <PoppinsText>Remove Freind</PoppinsText>
+        //         </TouchableOpacity>
+        //         {/* add friend button */}
+        //         <TouchableOpacity className='border-slate-700 border p-3 rounded w-48 items-center hover:bg-slate-600' onPress={() => FreindListChange(freinds)}>
+        //             <PoppinsText>Add Freind</PoppinsText>
+        //         </TouchableOpacity>
 
-                <PoppinsText weight="medium">{`we are user ${userData?.userToken}`}</PoppinsText>
-                {listItems.value &&
-                    <ContainerCol gap={1}>
-                        {listItems.value.map((item, index) => (
-                            <View key={index} className='p-2 bg-slate-700 rounded flex-row justify-between'>
-                                <PoppinsText weight="medium">{item}</PoppinsText>
-                                <TouchableOpacity className='bg-red-500 rounded-full w-6 h-6 items-center justify-center' onPress={() => removeItem(index)}>
-                                    <PoppinsText weight='bold'>-</PoppinsText>
-                                </TouchableOpacity>
-                            </View>
-                        ))}
+        //         <PoppinsText weight="medium">{`we are user ${userData?.userToken}`}</PoppinsText>
+        //         {listItems.value &&
+        //             <ContainerCol gap={1}>
+        //                 {listItems.value.map((item, index) => (
+        //                     <View key={index} className='p-2 bg-slate-700 rounded flex-row justify-between'>
+        //                         <PoppinsText weight="medium">{item}</PoppinsText>
+        //                         <TouchableOpacity className='bg-red-500 rounded-full w-6 h-6 items-center justify-center' onPress={() => removeItem(index)}>
+        //                             <PoppinsText weight='bold'>-</PoppinsText>
+        //                         </TouchableOpacity>
+        //                     </View>
+        //                 ))}
 
-                        <View className='w-full items-center'>
-                            <TouchableOpacity className='bg-slate-700 p-3 w-32 items-center' onPress={() => newItem()}>
-                                <PoppinsText>+</PoppinsText>
-                            </TouchableOpacity>
-                        </View>
+        //                 <View className='w-full items-center'>
+        //                     <TouchableOpacity className='bg-slate-700 p-3 w-32 items-center' onPress={() => newItem()}>
+        //                         <PoppinsText>+</PoppinsText>
+        //                     </TouchableOpacity>
+        //                 </View>
 
-                    </ContainerCol>
-                }
+        //             </ContainerCol>
+        //         }
 
-                <ContainerCol gap={1}>
-                    {freindsList?.map((freindList, index) => (
-                        <ContainerCol key={index}>
-                            <PoppinsText weight="medium">{freindList.userToken}</PoppinsText>
-                            {freindList.value?.map((item, itemIndex) => (
-                                <View key={itemIndex} className='p-2 bg-slate-700 rounded flex-row justify-between'>
-                                    <PoppinsText weight="medium">{item}</PoppinsText>
-                                </View>
-                            ))}
-                        </ContainerCol>
-                    ))}
-
-
-                </ContainerCol>
+        //         <ContainerCol gap={1}>
+        //             {freindsList?.map((freindList, index) => (
+        //                 <ContainerCol key={index}>
+        //                     <PoppinsText weight="medium">{freindList.userToken}</PoppinsText>
+        //                     {freindList.value?.map((item, itemIndex) => (
+        //                         <View key={itemIndex} className='p-2 bg-slate-700 rounded flex-row justify-between'>
+        //                             <PoppinsText weight="medium">{item}</PoppinsText>
+        //                         </View>
+        //                     ))}
+        //                 </ContainerCol>
+        //             ))}
 
 
-            </ContainerCol>
+        //         </ContainerCol>
 
-            <View className='w-full items-center'>
-                <TouchableOpacity className='bg-slate-700 p-3 w-32 items-center' onPress={() => signOut()}>
-                    <PoppinsText>Sign Out</PoppinsText>
-                </TouchableOpacity>
-            </View>
-        </View>
+
+        //     </ContainerCol>
+
+        //     <View className='w-full items-center'>
+        //         <TouchableOpacity className='bg-slate-700 p-3 w-32 items-center' onPress={() => signOut()}>
+        //             <PoppinsText>Sign Out</PoppinsText>
+        //         </TouchableOpacity>
+        //     </View>
+        // </View>
     );
 };
 
